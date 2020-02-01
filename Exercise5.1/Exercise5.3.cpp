d08b16550c975ae69e163b807b93b8f7400ab1e0
paul-aoun <59494483+paul-aoun@users.noreply.github.com>
2020-02-01 12:01:21 -0800

    Exercise 5.3

--- Exercise5.1/Exercise5.3.cpp
+++ Exercise5.1/Exercise5.3.cpp
@@ -3,13 +3,12 @@
 // CIS 554-M401 Object Oriented Programming in C++  //
 //													//
 // Syracuse University								//				
-// Exercise 5.2										//
+// Exercise 5.3										//
 //													//
 // 2/1/2020											//
 //												    //
-// This is file for the Exercise 5.2.				//
-// It demonstrates adding an argument with a default//
-// value.											//
+// This is file for the Exercise 5.3.				//
+// It demonstrates functions overloading.			//
 //////////////////////////////////////////////////////
 
 #include <iostream>
@@ -19,29 +18,43 @@ using std::endl;
 
 
 // Class that has a function to return the max value of
-// 4 integers. The fourth one is only included if it's
-// less than 1000000.
+// 3 or 4 integers. Demonstrates functions overloading.
 class Maximum
 {
 public:
-	// Compares 4 integers and returns the largest one. Num4 is included
-	// only if it's less than 1000000.
-	int GetMaximumValue(int num1, int num2, int num3, int num4 = 1000000)
+	// Compares 3 integers and returns the largest one.
+	int GetMaximumValue(int num1, int num2, int num3)
 	{
+		std::cout << "GetMaximumValue with 3 parameters was called \n";
+		
 		int maxValue = num1;
 		if (num2 > maxValue)
 			maxValue = num2;
 		if (num3 > maxValue)
 			maxValue = num3;
-		if ((num4 < 1000000) && (num4 > maxValue)) //Checks if num4 should be included in the comparaison
+		
+		return maxValue;
+	}
+
+	// Compares 4 integers and returns the largest one. 
+	int GetMaximumValue(int num1, int num2, int num3, int num4)
+	{
+		std::cout << "GetMaximumValue with 4 parameters was called \n";
+
+		int maxValue = num1;
+		if (num2 > maxValue)
+			maxValue = num2;
+		if (num3 > maxValue)
+			maxValue = num3;
+		if (num4 > maxValue)
 			maxValue = num4;
 
 		return maxValue;
 	}
 };
 
-//Driver function for Exercise 5.2.
-/*int main()
+//Driver function for Exercise 5.3.
+int main()
 {
 	int num1{ 0 };
 	int num2{ 0 };
@@ -57,13 +70,13 @@ public:
 	cout << "Enter third number: ";
 	cin >> num3;
 
-	// Calling function GetMaximumValue without the fourth parameter (old interface)
-	cout << "Max Value (without number 4) = " << max.GetMaximumValue(num1, num2, num3) << endl;
+	// Calling function GetMaximumValue with 3 parameters.
+	cout << "Max Value (with 3 parameters) = " << max.GetMaximumValue(num1, num2, num3) << endl;
 
 	cout << "Enter fourth number: ";
 	cin >> num4;
-	// Calling function GetMaximumValue with the fourth parameter (new interface)
+	// Calling function GetMaximumValue with 4 parameters.
 	cout << "Max Value (with number 4) = " << max.GetMaximumValue(num1, num2, num3, num4) << endl;
 
 	return 0;
-}*/
+}
